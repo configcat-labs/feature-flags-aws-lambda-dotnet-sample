5db2563c26b1e989a5dcbde698576cc38b2f2d0d
Adam Simon <adamosimoni@gmail.com>
2026-08-28 14:55:04 +0200

    Integrate ConfigCat

--- Function.cs
+++ Function.cs
@@ -1,21 +1,42 @@
 using Amazon.Lambda.Core;
+using ConfigCat.Client;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace ConfigCatLambdaDemo;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    private static readonly IConfigCatClient _configCatClient;
+
+    static Function()
     {
-        return input.ToUpper();
+        var sdkKey = "YOUR-CONFIGCAT-SDK-KEY";
+
+        // Credentials to connect to the local Redis server
+        var redisConnectionString = "localhost:6379,password=myRedisPassword123,user=default";
+
+        // Configure the ConfigCat client to use the Redis cache
+        _configCatClient = ConfigCatClient.Get(sdkKey, options =>
+        {
+            options.ConfigCache = new RedisConfigCatCache(redisConnectionString);
+            options.PollingMode = PollingModes.LazyLoad(cacheTimeToLive: TimeSpan.FromSeconds(60));
+        });
+    }
+
+    public static async Task<string> FunctionHandler(ILambdaContext context)
+    {
+        // Ensure the flag key here matches the one in your ConfigCat Dashboard
+        var isFeatureEnabled = await _configCatClient.GetValueAsync("myFeatureFlag", false);
+
+        // Log the feature flag value
+        context.Logger.LogInformation($"ConfigCat Flag value: {isFeatureEnabled}");
+
+        if (isFeatureEnabled)
+        {
+            return "New Feature Logic Enabled";
+        }
+
+        return "Old Feature Active";
     }
 }
---
+++ RedisConfigCatCache.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.Lambda.Core;
+using ConfigCat.Client;
+using StackExchange.Redis;
+
+namespace ConfigCatLambdaDemo;
+
+public class RedisConfigCatCache : IConfigCatCache
+{
+    private readonly Lazy<ConnectionMultiplexer> _connection;
+
+    public RedisConfigCatCache(string connectionString)
+    {
+
+        var options = ConfigurationOptions.Parse(connectionString);
+        options.AbortOnConnectFail = false;
+        options.ConnectTimeout = 2000;
+        options.SyncTimeout = 2000;
+
+        _connection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(options));
+    }
+
+    private IDatabase Database => _connection.Value.GetDatabase();
+
+    public async ValueTask<string?> GetAsync(string key, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            return await Database.StringGetAsync(key);
+        }
+        catch (RedisException e)
+        {
+            // Log get warning 
+            LambdaLogger.Log($"Error getting redis cache value: {e}");
+            return null;
+        }
+    }
+
+    public async ValueTask SetAsync(string key, string value, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await Database.StringSetAsync(key, value);
+        }
+        catch (RedisException e)
+        {
+            // Log set warning
+            LambdaLogger.Log($"Error setting redis cache value: {e}");
+        }
+    }
+}
